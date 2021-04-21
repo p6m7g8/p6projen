73e4497d3291bdb4652e74a3e343b0701e045b02
Philip M. Gollucci <pgollucci@p6m7g8.com>
2021-04-21 19:06:32 -0400

    fix: double p6m7g8/p6m7g8

--- init.zsh
+++ init.zsh
@@ -23,7 +23,7 @@ p6df::modules::p6projen::deps() {
 ######################################################################
 p6df::modules::p6projen::init() {
 
-  local dir="$P6_DFZ_SRC_P6M7G8_DIR/p6m7g8/p6projen"
+  local dir="$P6_DFZ_SRC_P6M7G8_DIR/p6projen"
 
   p6_bootstrap "$dir"
 
